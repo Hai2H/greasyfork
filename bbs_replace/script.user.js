52257902cd11cb4bbe0dc5babef637043aa1b801
Pyy <able_hua@163.com>
2025-01-14 14:28:08 +0800

    【修改】更新匹配网站

--- bbs_replace/script.user.js
+++ bbs_replace/script.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         社区内容替换
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.1.1
 // @license      MIT
 // @description  BBS社区内容替换
 // @author       PYY
-// @match        https://www.kuafuzys.com/thread-create-*.htm
-// @match        https://bbs.52huahua.cc/?thread-create-*.htm
-// @match        https://www.kuafuzys.com/post-update-*.htm
+// @match        https://www.kuafuzys.com/*
+// @match        https://bbs.52huahua.cc/*
+// @match        https://www.kuafuzy.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=kuafuzys.com
 // ==/UserScript==
 
