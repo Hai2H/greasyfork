00e0d902673b76e6b68fdd09a9a77d122eb3b4d9
Pyy <able_hua@163.com>
2025-03-27 10:15:05 +0800

    【添加】夸克推广自动查询 - 修改匹配规则

--- quark_promotion/scipt.user.js
+++ quark_promotion/scipt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         夸克项目推广查询
 // @namespace    http://tampermonkey.net/
-// @version      1.0.3
+// @version      1.0.4
 // @license      MIT
 // @description  夸克项目推广查询!
 // @author       PYY
@@ -14,26 +14,29 @@
 (function() {
     'use strict';
 
-    // 要填写的夸克 UID
-    const quarkUID = '100188018441';  // 请将此值替换为实际的夸克 UID
+    window.addEventListener('load', function() {
 
-    const inputElement = document.querySelector('input[placeholder="请输入夸克UID查询"]');
-    if (inputElement) {
-        inputElement.value = quarkUID;
-        // 触发 input 事件让 Vue 感知到输入框内容的变化
-        const inputEvent = new Event('input', { bubbles: true });
-        inputElement.dispatchEvent(inputEvent);
-    }
+        // 要填写的夸克 UID
+        const quarkUID = '100188018441';  // 请将此值替换为实际的夸克 UID
+
+        const inputElement = document.querySelector('input[placeholder="请输入夸克UID查询"]');
+        if (inputElement) {
+            inputElement.value = quarkUID;
+            // 触发 input 事件让 Vue 感知到输入框内容的变化
+            const inputEvent = new Event('input', {bubbles: true});
+            inputElement.dispatchEvent(inputEvent);
+        }
 
 
-    // 找到查询 div 并触发点击事件
-    const submitDiv = document.querySelector('.submit');
-    if (submitDiv) {
-        const clickEvent = new MouseEvent('click', {
-            view: window,
-            bubbles: true,
-            cancelable: true
-        });
-        submitDiv.dispatchEvent(clickEvent);
-    }
+        // 找到查询 div 并触发点击事件
+        const submitDiv = document.querySelector('.submit');
+        if (submitDiv) {
+            const clickEvent = new MouseEvent('click', {
+                view: window,
+                bubbles: true,
+                cancelable: true
+            });
+            submitDiv.dispatchEvent(clickEvent);
+        }
+    });
 })();
